56bf4f3d749418271d6849b66d1a9622d2edf054
karthik <ishowcomplex16@gmail.com>
2026-07-26 16:07:06 +0530

    Created api gateway & service registry

--- BMS Clone - Micro-Services/api-gateway/src/main/java/com/bookmyshow/api_gateway/ApiGatewayApplication.java
+++ BMS Clone - Micro-Services/api-gateway/src/main/java/com/bookmyshow/api_gateway/ApiGatewayApplication.java
@@ -1,17 +1,15 @@
-package com.bookmyshow.catalogservice;
+package com.bookmyshow.api_gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableCaching
 @EnableDiscoveryClient
-public class CatalogServiceApplication {
+public class ApiGatewayApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CatalogServiceApplication.class, args);
+		SpringApplication.run(ApiGatewayApplication.class, args);
 	}
 
 }
