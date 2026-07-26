da7704d8e385d1cf88ca71ede298d8ecbbf9fced
KEVINHARRI2002 <kevinharrisleon@gmail.com>
2026-07-26 18:01:54 +0530

    Added Cors Configuration

--- BMS Clone - Micro-Services/catalog-service/src/main/java/com/bookmyshow/catalogservice/CatalogServiceApplication.java
+++ BMS Clone - Micro-Services/catalog-service/src/main/java/com/bookmyshow/catalogservice/CatalogServiceApplication.java
@@ -6,7 +6,6 @@ import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableCaching
 @EnableDiscoveryClient
 public class CatalogServiceApplication {
 
